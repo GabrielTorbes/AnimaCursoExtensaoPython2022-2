e9e57a2168ea466a12baec118524624b60991603
goleiro22 <116231223+GabrielTorbes@users.noreply.github.com>
2022-11-16 23:31:22 +0000

    Aula 4

---
+++ Aula4-2022.py
@@ -0,0 +1,19 @@
+#1o. passo: Importar a biblioteca sqlite3
+import sqlite3
+
+#2o. passo: Vamos estabelecer um conexão com o banco de dados
+conexao = sqlite3.connect("dc_universe.db")
+
+#3o. passo: criar um objeto do tipo cursor
+cursor = conexao.cursor()
+
+#4o. passo: Comando SQL do banco
+sql = "SELECT pessoa_id, nome, nome_civil, tipo FROM pessoas"
+
+#5o. passo: Executar o comando SQL no SQLlite(No cursor)
+cursor.execute(sql)
+
+#6o. passo: Exibir a consulta com todos os nomes de heróis e vilões dp banco de dados
+pessoas = cursor.fetchall()
+for pessoa in pessoas:
+  print(pessoa)
